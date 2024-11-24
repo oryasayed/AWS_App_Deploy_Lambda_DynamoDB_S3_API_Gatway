90bd1f7badb42d85ae6d84f6af66318e4a396e09
oryasayed <94091919+oryasayed@users.noreply.github.com>
2024-11-23 18:12:04 -0700

    Update scripts.js
    api_endpoint changed

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 // Add your API endpoint here
-var API_ENDPOINT = "API_ENDPOIND_PASTE_HERE";
+var API_ENDPOINT = "https://7gf50dafkl.execute-api.us-east-1.amazonaws.com/Prod";
 
 // AJAX POST request to save student data
 document.getElementById("savestudent").onclick = function(){
